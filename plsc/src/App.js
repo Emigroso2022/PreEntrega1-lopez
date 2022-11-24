315a61ca5d968549d19482c0a5504ec5f06ab92e
Emiliano <dalupsystems.2021@gmail.com>
2022-11-23 22:20:27 -0300

    first commit

---
+++ plsc/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar/>
+     <h1>Ventas Apple </h1>
+
+    </div>
+   
+
+  );
+}
+
+export default App;
---
+++ plsc/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+
+import './Navbar.css'
+
+
+const  Nvbar = () => {
+
+    return(
+        <nav calssName='navbar'>
+            <h1>Apple</h1>
+            <button calssName='boton'>Inicio </button>
+            <button calssName='boton'>Productos </button>
+            <button calssName='boton'>Contacto </button>
+            <button calssName='boton'>Nosotos </button>
+        </nav>
+
+    )
+}
+
+export default Nvbar
